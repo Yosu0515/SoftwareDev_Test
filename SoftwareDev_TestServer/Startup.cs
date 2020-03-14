25f279733278bd7632813ceb5a42b3ef2b9f2cd3
yosu0515 <josdevries1989@live.nl>
2020-03-14 18:53:11 +0100

    Some unpushed code for whatever reason

--- SoftwareDev_TestServer/Startup.cs
+++ SoftwareDev_TestServer/Startup.cs
@@ -1,18 +1,15 @@
 using System;
-using System.Collections.Generic;
 using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Text.Json;
-using System.Text.Json.Serialization;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace SoftwareDev_TestServer
 {
@@ -37,12 +34,25 @@ namespace SoftwareDev_TestServer
 
             app.Use(async (context, next) =>
             {
-                if (context.Request.Path == "/ws")
+                if (context.Request.Path == "/simulation")
                 {
                     if (context.WebSockets.IsWebSocketRequest)
                     {
                         var webSocket = await context.WebSockets.AcceptWebSocketAsync();
-                        await Echo(context, webSocket);
+                        await SimulationResponse(context, webSocket);
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 400;
+                    }
+                }
+                else if (context.Request.Path == "/controller") 
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                        // TODO: Make a ControllerResponse
+                        await SimulationResponse(context, webSocket);
                     }
                     else
                     {
@@ -56,9 +66,10 @@ namespace SoftwareDev_TestServer
             });
         }
 
-        private static async Task Send(byte[] buffer, WebSocket webSocket, WebSocketReceiveResult result)
+        private static async Task Send(byte[] buffer, WebSocket webSocket, WebSocketReceiveResult result, StringBuilder stringBuilder)
         {
-            const string returnString = "Received, Thank you!";
+            string returnString = stringBuilder.ToString();
+            
             var buffer2 = Encoding.UTF8.GetBytes(returnString);
 
             await webSocket.SendAsync(
@@ -68,58 +79,46 @@ namespace SoftwareDev_TestServer
                     CancellationToken.None);
         }
 
-        private static async Task Echo(HttpContext context, WebSocket webSocket)
+        private static async Task SimulationResponse(HttpContext context, WebSocket webSocket)
         {
             var buffer = new byte[1024 * 4];
             var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
 
             var converted = Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+            Console.WriteLine("Json Received: " + converted);
+
+            Simulation simulation = JsonConvert.DeserializeObject<Simulation>(converted);
+
+            StringBuilder stringBuilder = CheckSimulationValidation(simulation);
+            await Send(buffer, webSocket, result, stringBuilder);
             
-            while (!result.CloseStatus.HasValue)
-            {
-                await Send(buffer, webSocket, result);
-                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-                
-                var json = JsonConvert.DeserializeObject<TrafficLightModel>(converted);
-                ConsoleTests(json);
-                
-                Console.WriteLine("Json Received: " + converted);
-            }
-            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+            if (result.CloseStatus != null)
+                await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription,
+                    CancellationToken.None);
         }
 
-        //TODO replace console test with proper NUnit tests
-        private static void ConsoleTests(TrafficLightModel trafficLightModel)
+        private static StringBuilder CheckSimulationValidation(Simulation sim)
         {
-            if (trafficLightModel.A1 == null)
+            SimulationValidator val = new SimulationValidator(sim);
+            ValidationResult validationResult = val.Validate(sim);
+            StringBuilder stringBuilder = new StringBuilder();
+            
+            if (!validationResult.IsValid)
             {
-                Console.WriteLine("A1 is empty!");
+                foreach (var error in validationResult.Errors)
+                {
+                    Console.WriteLine("Property: " + error.PropertyName + " failed validation. Error: " + error.ErrorMessage);
+                    stringBuilder.Append("Property: " + error.PropertyName + " failed validation. Error: " +
+                                         error.ErrorMessage);
+                }
             }
             else
             {
-                Console.WriteLine("A1 set to value: " + trafficLightModel.A1);
+                Console.WriteLine("Validation passed!");
+                stringBuilder.Append("Validation passed!");
             }
-            if (trafficLightModel.A2 == null)
-            {
-                Console.WriteLine("A2 is empty!");
-            }else
-            {
-                Console.WriteLine("A2 set to value: " + trafficLightModel.A2);
-            }
-            if (trafficLightModel.A3 == null)
-            {
-                Console.WriteLine("A3 is empty!");
-            }else
-            {
-                Console.WriteLine("A3 set to value: " + trafficLightModel.A3);
-            }
-            if (trafficLightModel.A4 == null)
-            {
-                Console.WriteLine("A4 is empty!");
-            }else
-            {
-                Console.WriteLine("A4 set to value: " + trafficLightModel.A4);
-            }
+
+            return stringBuilder;
         }
     }
 }
